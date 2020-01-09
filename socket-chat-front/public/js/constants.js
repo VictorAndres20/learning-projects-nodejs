3288e6881ad7b6c8d7a2a5a8454678553b98fe5c
victorandres20 <vapedraza1706@gmail.com>
2020-01-08 20:31:22 -0500

    Storage front chat

---
+++ socket-chat-front/public/js/constants.js
@@ -0,0 +1,3 @@
+const USER_ID = 'name';
+const USERS_ID = 'users';
+const CHATS_ID = 'chats';
--- socket-chat-front/public/js/storage.js
+++ socket-chat-front/public/js/storage.js
@@ -1,11 +1,73 @@
+function addUserConnected(id, name){
+    let users = getObject(USERS_ID);
+    users.push({id, name});
+    return saveObject(USERS_ID,users);
+}
+
+function addChat(name, ownerMessage, message){
+    let chats = getObject(CHATS_ID);
+    let chatsFiltered = chats.filter((chat) => chat.name === name);
+    if(chatsFiltered.length !== 0){
+        let newChat = chatsFiltered[0];
+        newChat.chats.push(buildMessage(ownerMessage, message));
+        chats = removeChat(name);
+        chats.push(newChat);
+        chats = saveObject(CHATS_ID, chats);
+    }    
+    return chats;
+}
+
+function removeUserConnected(name){
+    let users = getObject(USERS_ID);
+    users.map((user, key) => {
+        if(user.name === name)
+            users.splice(key, 1);
+    });
+    return saveObject(USERS_ID,users);
+}
+
+function removeChat(name){
+    let chats = getObject(CHATS_ID);
+    chats.map((chat , key) => {
+        if(chat.name === name)
+            chats.splice(key, 1);
+    });
+    return saveObject(CHATS_ID, chats);
+}
+
+function getChat(name){
+    let chats = getObject(CHATS_ID);
+    chats = chats.filter((chat) => chat.name === name);
+    if(chats.length === 0)
+        return [];
+    return chats[0].chats;
+}
+
+function buildEmpyChat(id, name){
+    return {id, name, chats: []};
+}
+
+function saveObject(id, data){
+    sessionStorage.setItem(id, JSON.stringify(data));
+    return data;
+}
+
+function getObject(){
+    return JSON.parse(sessionStorage.getItem(id));
+}
+
+function removeObject(){
+    sessionStorage.removeItem(id);
+}
+
 function saveUser(name){
-    sessionStorage.setItem('name', name);
+    sessionStorage.setItem(USER_ID, name);
 }
 
 function getUser(){
-    sessionStorage.getItem('name');
+    return sessionStorage.getItem(USER_ID);
 }
 
 function removeUser(){
-    sessionStorage.removeItem('name');
+    sessionStorage.removeItem(USER_ID);
 }
